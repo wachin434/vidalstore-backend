1d9af6b8d6f8fcc2eaf664feb1709359aec683de
Felipe <fe.ortegah@duocuc.cl>
2026-09-14 23:00:24 -0300

    feat: configura CORS y arranque del gateway

---
+++ gateway/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { CatalogoController } from './catalogo/catalogo.controller';
+import { ComprasController } from './compras/compras.controller';
+import { BibliotecaController } from './biblioteca/biblioteca.controller';
+import { LicenciasController } from './licencias/licencias.controller';
+import { AuditoriaController } from './auditoria/auditoria.controller';
+
+@Module({
+  controllers: [
+    CatalogoController,
+    ComprasController,
+    BibliotecaController,
+    LicenciasController,
+    AuditoriaController,
+  ],
+})
+export class AppModule {}
