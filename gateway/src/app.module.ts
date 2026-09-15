fdcd3a68e8fb962e0411cf35be9f3c4a804d2ba3
Antonio-cornej <email-de-antonio@gmail.com>
2026-09-14 23:46:04 -0300

    feat: registra los controladores de catalogo, compras y biblioteca

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -5,6 +5,7 @@ import { BibliotecaController } from './biblioteca/biblioteca.controller';
 import { LicenciasController } from './licencias/licencias.controller';
 import { AuditoriaController } from './auditoria/auditoria.controller';
 
+
 @Module({
   controllers: [
     CatalogoController,
